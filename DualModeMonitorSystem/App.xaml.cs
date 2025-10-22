f18bd74bef23ff4685ea3e914cb953d16bf2bea5
爽 冷 <lsisloser@gmail.com>
2025-10-22 16:44:42 +0800

    添加项目文件。

---
+++ DualModeMonitorSystem/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DualModeMonitorSystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return MainWindow;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+
+}
